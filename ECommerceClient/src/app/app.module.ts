1852389723fe2595998f7753cbc28e8b5be6bf1a
Eren Çalbay <erencalbayfb@hotmail.com>
2023-10-26 15:41:29 +0300

    Feat: Angular Material infra added.

--- ECommerceClient/src/app/app.module.ts
+++ ECommerceClient/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
 import { UIModule } from './ui/ui.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { UIModule } from './ui/ui.module';
     BrowserModule,
     AppRoutingModule,
     AdminModule,
-    UIModule
+    UIModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
