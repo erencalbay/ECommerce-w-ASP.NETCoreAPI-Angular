15423e5588199283a79561bf2c0e477720ef8832
Eren Çalbay <erencalbayfb@hotmail.com>
2023-11-30 14:23:32 +0300

    Feat: Item adding operation-AdminProductService UI

--- ECommerceClient/src/app/admin/components/products/products.component.ts
+++ ECommerceClient/src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -14,33 +13,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
     super(spinner)
   }
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
-
-    /*this.httpClientService.post({
-      controller: "products"
-    },{
-      name: "kagit",
-      stock: 1000,
-      price: 5
-
-    }).subscribe();*/
-
-    /*this.httpClientService.put({
-      controller: "products",
-    }, {
-      id : "231370cd5-8050-4d80-95f6-d38e4b85555d",
-      name: "Renkli Kalemi",
-      stock: 120,
-      price: 20
-
-    }).subscribe();*/
-
-    /*this.httpClientService.delete({
-      controller : "products",
-    }, "22d1155e-3d8f-46f0-a331-aceac0d9d176").subscribe();*/
 
   }
 }
---
+++ ECommerceClient/src/app/services/common/models/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClientService } from '../http-client.service';
+import { Create_Product } from 'src/app/contracts/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private httpClientService: HttpClientService) { }
+  create(product: Create_Product, successCallBack?: any) {
+    this.httpClientService.post({
+      controller: "products"
+    }, product)
+    .subscribe(result => {
+      successCallBack();
+    });
+  }
+}
